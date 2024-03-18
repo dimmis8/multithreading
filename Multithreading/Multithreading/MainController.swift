11ddf69bb66483bace883c74bd10341164f80ed0
Dmirty <dimmis8@yandex.ru>
2024-03-18 14:35:08 +0300

    Проект создан

---
+++ Multithreading/Multithreading/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  MainController.swift
+//  Multithreading
+//
+//  Created by Дмитрий Логвинов on 18.03.2024.
+//
+
+import UIKit
+
+/// Меню выбора задач
+final class MainController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Multithreading/Multithreading/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Multithreading
+//
+//  Created by Дмитрий Логвинов on 18.03.2024.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
